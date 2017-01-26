54798237553eb9ca85a7cc2ba2e0f9b130c5491b
Lody Borgers <hi@lodybo.nl>
2017-01-27 00:23:03 +0100

    Created the sendmail service for.. sending mails

--- app.js
+++ app.js
@@ -1,7 +1,48 @@
 angular.module("sesApp", ["ngResource"])
-    .controller("sesCtrl", function ($scope, $resource) {
+    .controller("sesCtrl", function ($scope, sesEmailService) {
         $scope.toggles = {
-            success: true,
-            error: true
+            success: false,
+            error: false
         };
+
+        $scope.form = {
+            name: "",
+            email: "",
+            subject: "",
+            message: ""
+        };
+
+        $scope.submitForm = function () {
+            $scope.toggles = {
+                success: false,
+                error: false
+            };
+
+            sesEmailService.sendMail(form).then(function (response) {
+                $scope.toggles.success = true;
+            }).catch(function (error) {
+                $scope.toggles.error = true;
+            });
+        };
+    })
+    .service("sesEmailService", function ($resource) {
+        function __sendMail(mail) {
+            $resource("url", {
+                name: "@name",
+                subject: "@subject",
+                email: "@email",
+                message: "@message"
+            });
+
+            return $resource.POST({
+                name: mail.name,
+                subject: mail.subject,
+                email: mail.email,
+                message: mail.message
+            });
+        }
+
+        return {
+            sendMail: __sendMail
+        }
     });
